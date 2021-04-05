b0e42af829aafc669373c6ffe9cdf11abaa39737
Merlin <mquirosdonate@gmail.com>
2021-04-05 17:25:19 +0200

    20200405-17:24

--- app/src/main/java/Files/FileChooser.java
+++ app/src/main/java/Files/FileChooser.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.xml.R;
 
@@ -16,27 +19,43 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class FileChooser extends ListActivity {
+public class FileChooser extends AppCompatActivity {
     private File currentDir;
     private FileArrayAdapter adapter;
+    ListView listView;
+    private File InternalStorage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Ruta de la sdCard
-        //Bundle b = this.getIntent().getExtras();
-        //String sCurrentDir = b.getString("SDDIR");
-        //if (sCurrentDir.isEmpty()) {
-            currentDir = Environment.getExternalStorageDirectory();
-        //} else {
-        //    currentDir = new File(sCurrentDir);
-        //}
+        setContentView(R.layout.activity_view_file_chooser);
+        currentDir = new File(getIntent().getStringExtra("SDDIR"));
+        InternalStorage = Environment.getExternalStorageDirectory();
+
+        listView = (ListView) findViewById(R.id.listViewFiles);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Item o = adapter.getItem(position);
+                if(o.getImage().equalsIgnoreCase("directory_icon")||o.getImage().equalsIgnoreCase("directory_up")){
+                    try {
+                        currentDir = new File(o.getPath());
+                        fill(currentDir);
+                    }catch (Exception e) {
+                        fill(InternalStorage);
+                    }
+                } else  {
+                    onFileClick(o);
+                }
+            }
+        });
         fill(currentDir);
     }
     private void fill(File f) {
+        if(f == null)return;
         File[]dirs = f.listFiles();
-        this.setTitle("Current Dir: "+f.getName());
+        getSupportActionBar().setTitle("Dir: "+f.getName());
         List<Item> dir = new ArrayList<Item>();
-        List<Item>fls = new ArrayList<Item>();
+        List<Item> fls = new ArrayList<Item>();
         try{
             for(File ff: dirs)
             {
@@ -62,34 +81,17 @@ public class FileChooser extends ListActivity {
                     fls.add(new Item(ff.getName(),ff.length() + " Byte", date_modify, ff.getAbsolutePath(),"file_icon"));
                 }
             }
-        }catch(Exception e)
-        {
-
-        }
+        }catch(Exception e) { }
         Collections.sort(dir);
         Collections.sort(fls);
         dir.addAll(fls);
         if(!f.getName().equalsIgnoreCase("sdcard"))
-            dir.add(0,new Item("..","Parent Directory","",f.getParent(),"directory_up"));
-        adapter = new FileArrayAdapter(FileChooser.this, R.layout.file_view,dir);
-        this.setListAdapter(adapter);
+            dir.add(0,new Item("","","",f.getParent(),"directory_up"));
+        adapter = new FileArrayAdapter(Files.FileChooser.this, R.layout.list_files,dir);
+        listView.setAdapter(adapter);
     }
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        // TODO Auto-generated method stub
-        super.onListItemClick(l, v, position, id);
-        Item o = adapter.getItem(position);
-        if(o.getImage().equalsIgnoreCase("directory_icon")||o.getImage().equalsIgnoreCase("directory_up")){
-            currentDir = new File(o.getPath());
-            fill(currentDir);
-        }
-        else
-        {
-            onFileClick(o);
-        }
-    }
+
     private void onFileClick(Item o){
-        //Toast.makeText(this, "Folder Clicked: "+ currentDir, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent();
         intent.putExtra("GetPath",currentDir.toString());
         intent.putExtra("GetFileName",o.getName());
